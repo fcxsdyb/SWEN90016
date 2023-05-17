7a38f7024e5df4becd9903e8f089f7b4c7dc3852
zyh <zhouyu@kean.edu>
2023-05-17 12:11:40 +1000

    update

--- Backend/src/RecipientController.java
+++ Backend/src/RecipientController.java
@@ -12,41 +12,50 @@ public class RecipientController {
 
             // Open a connection
             System.out.println("Connecting to database...");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_name", "username", "password");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/VR1Family", "root", "qzzyh0808");
 
             // Execute a query
             System.out.println("Creating statement...");
             stmt = conn.createStatement();
             String sql;
-            sql = "SELECT id, field1, field2 FROM table_name";
+            sql = "SELECT recipient_id, name, age, previous_address, total_family_members, partner_name, partner_age FROM Recipients";
             rs = stmt.executeQuery(sql);
 
             // Extract data from result set
-            while(rs.next()){
+            while (rs.next()) {
                 //Retrieve by column name
-                int id  = rs.getInt("id");
-                String field1 = rs.getString("field1");
-                String field2 = rs.getString("field2");
+                int id = rs.getInt("recipient_id");
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+                String previous_address = rs.getString("previous_address");
+                int total_family_members = rs.getInt("total_family_members");
+                String partner_name = rs.getString("partner_name");
+                int partner_age = rs.getInt("partner_age");
+
 
                 //Display values
                 System.out.print("ID: " + id);
-                System.out.print(", Field1: " + field1);
-                System.out.println(", Field2: " + field2);
+                System.out.print(", Name: " + name);
+                System.out.print(", Age: " + age);
+                System.out.print("previous_address: " + previous_address);
+                System.out.print(", total_family_members: " + total_family_members);
+                System.out.print(", partner_name: " + partner_name);
+                System.out.print("partner_age: " + partner_age);
             }
-        } catch(Exception e){
+        } catch (Exception e) {
             //Handle errors for Class.forName
             e.printStackTrace();
-        } finally{
+        } finally {
             //finally block used to close resources
-            try{
-                if(stmt!=null)
+            try {
+                if (stmt != null)
                     stmt.close();
-            }catch(SQLException se2){
+            } catch (SQLException se2) {
             }// nothing we can do
-            try{
-                if(conn!=null)
+            try {
+                if (conn != null)
                     conn.close();
-            }catch(SQLException se){
+            } catch (SQLException se) {
                 se.printStackTrace();
             }//end finally try
         }//end try
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import InformationPage from './pages/InformationPage';
-import './App.css';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ textAlign: 'center' }}>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/information" element={<InformationPage />} />
